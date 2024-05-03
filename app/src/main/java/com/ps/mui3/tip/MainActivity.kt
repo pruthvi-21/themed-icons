c1de1b9e17549ab764f5d66d47ee24a57a553a51
Pruthvi Bugidi <pruthvi.bugidi@proton.me>
2024-05-03 13:40:59 +0530

    Added searchbar to search icons

--- app/src/main/java/com/ps/mui3/tip/MainActivity.kt
+++ app/src/main/java/com/ps/mui3/tip/MainActivity.kt
@@ -9,30 +9,58 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
+import androidx.core.view.isVisible
+import androidx.core.widget.doOnTextChanged
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.search.SearchBar
+import com.google.android.material.search.SearchView
 import com.ps.mui3.tip.icons.IconInfo
 import com.ps.mui3.tip.icons.IconsHelper
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var searchBar: SearchBar
+    private lateinit var searchView: SearchView
+    private lateinit var noSearchResultsView: TextView
+    private lateinit var searchResultCount: TextView
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var searchRecyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
-        setSupportActionBar(toolbar)
+        searchBar = findViewById(R.id.search_bar)
+        searchView = findViewById(R.id.search_view)
+        noSearchResultsView = findViewById(R.id.no_search_results)
+        searchResultCount = findViewById(R.id.search_result_count)
+        recyclerView = findViewById(R.id.recycler_view)
+        searchRecyclerView = findViewById(R.id.search_recycler_view)
 
         val allIcons = IconsHelper.getIconsList(this)
+        val allIconsAdapter = RVAdapter(this, allIcons)
+        val searchIconsAdapter = RVAdapter(this, allIcons)
 
-        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
         recyclerView.layoutManager = GridLayoutManager(this, 4)
-        recyclerView.adapter = RVAdapter(this, allIcons)
+        recyclerView.adapter = allIconsAdapter
+
+        searchRecyclerView.layoutManager = GridLayoutManager(this, 4)
+        searchRecyclerView.adapter = searchIconsAdapter
+        searchIconsAdapter.updateList("")
+
+        searchBar.hint = getString(R.string.search_bar_hint, allIcons.size)
+
+        searchView.setupWithSearchBar(searchBar)
+        searchView.editText.doOnTextChanged { text, _, _, _ ->
+            val filteredCount = searchIconsAdapter.updateList(text.toString())
+            noSearchResultsView.isVisible = filteredCount == 0
+            searchResultCount.text = getString(R.string.search_result_count, filteredCount)
+        }
     }
 
     class RVAdapter(
@@ -40,14 +68,17 @@ class MainActivity : AppCompatActivity() {
         private val icons: List<IconInfo>
     ) : RecyclerView.Adapter<RVAdapter.RVViewHolder>() {
 
+        private var layoutInflater = LayoutInflater.from(context)
+        private var filteredIcons: MutableList<IconInfo> = icons.toMutableList()
+
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RVViewHolder {
-            return RVViewHolder(LayoutInflater.from(context).inflate(R.layout.tile, null, false))
+            return RVViewHolder(layoutInflater.inflate(R.layout.tile, parent, false))
         }
 
-        override fun getItemCount() = icons.size
+        override fun getItemCount() = filteredIcons.size
 
         override fun onBindViewHolder(holder: RVViewHolder, position: Int) {
-            val icon = icons[position]
+            val icon = filteredIcons[position]
 
             holder.itemView.setOnClickListener {
                 buildDialog(context, icon).show()
@@ -58,7 +89,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         private fun buildDialog(context: Context, iconInfo: IconInfo): AlertDialog {
-            val dialogView = LayoutInflater.from(context).inflate(R.layout.dialog_icon_info, null)
+            val dialogView = layoutInflater.inflate(R.layout.dialog_icon_info, null)
 
             val iconView: ImageView = dialogView.findViewById(R.id.icon)
             val iconLabel: TextView = dialogView.findViewById(R.id.icon_label)
@@ -82,9 +113,23 @@ class MainActivity : AppCompatActivity() {
             Glide.with(context)
                 .load(drawableRes)
                 .skipMemoryCache(true)
-                .transition(DrawableTransitionOptions.withCrossFade(300))
+                .transition(DrawableTransitionOptions.withCrossFade(150))
                 .diskCacheStrategy(DiskCacheStrategy.NONE)
                 .into(imageView)
+        }
+
+        fun updateList(filter: String): Int {
+            filteredIcons.clear()
+
+            if (filter.isNotEmpty()) {
+                icons.forEach {
+                    if (it.label.contains(filter.trim(), true))
+                        filteredIcons.add(it)
+                }
+            }
+
+            notifyDataSetChanged()
+            return filteredIcons.size
         }
 
         class RVViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
